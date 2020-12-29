a67f7d809e6a802f588d183ac7f4d7eba70b90e0
Rakesh <rakesh.subudhi50@gmail.com>
2020-12-29 11:44:18 +0530

    DependencyLookUpWithSetter

---
+++ DependencyLookUpWithSetter/src/main/java/com/rk/bean/Engine.java
@@ -0,0 +1,23 @@
+package com.rk.bean;
+
+public class Engine {
+
+	//constructor
+	public Engine()
+	{
+		System.out.println("0param constructor in Engine class:::: ");
+	}
+	
+	//method for Start method
+	public void startEngine()
+	{
+		System.out.println("Engine.Start() engineeeeeee::::::::::::::: ");
+	}
+	
+	//method for Stop Method
+	public void stopEngine()
+	{
+		System.out.println("Engine.Stop() engineeeeeee::::::::::::::: ");
+	}
+	
+}//class
